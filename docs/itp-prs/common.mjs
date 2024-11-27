a7ba43d6d81974aee9975553f449b5c26e131081
Daniel Wagner-Hall <dawagner@gmail.com>
2024-11-27 11:47:39 +0000

    Display recent review frequency

--- docs/itp-prs/common.mjs
+++ docs/itp-prs/common.mjs
@@ -1,4 +1,4 @@
-function daysToMilliseconds(days) {
+export function daysToMilliseconds(days) {
     return days * 24 * 60 * 60 * 1000;
 }
 
--- docs/itp-prs/reviewers.mjs
+++ docs/itp-prs/reviewers.mjs
@@ -1,9 +1,10 @@
-import { fetchPrsWithComments } from "./common.mjs";
+import { daysToMilliseconds, fetchPrsWithComments } from "./common.mjs";
 
 class ReviewerDetails {
-    constructor(prs, latestCommentTime) {
+    constructor(prs, latestCommentTime, recentReviewDays) {
         this.prs = prs;
         this.latestCommentTime = latestCommentTime;
+        this.recentReviewDays = recentReviewDays
     }
 }
 
@@ -15,13 +16,16 @@ async function onLoad() {
         for (const comment of pr.comments) {
             if (!comment.isPrAuthor) {
                 if (!(comment.userName in reviewers)) {
-                    reviewers[comment.userName] = new ReviewerDetails(new Set([pr.url]), comment.createdAt);
+                    reviewers[comment.userName] = new ReviewerDetails(new Set([pr.url]), comment.createdAt, new Set());
                 } else {
                     const reviewer = reviewers[comment.userName];
                     reviewer.prs.add(pr.url);
                     if (comment.createdAt > reviewer.latestCommentTime) {
                         reviewer.latestCommentTime = comment.createdAt;
                     }
+                }
+                if (new Date() - comment.createdAt < daysToMilliseconds(28)) {
+                    reviewers[comment.userName].recentReviewDays.add(toIsoDate(comment.createdAt));
                 }
             }
         }
@@ -40,8 +44,9 @@ async function onLoad() {
         card.querySelector(".username").href = `https://github.com/${userName}`;
         card.querySelector(".last-review").innerText = toIsoDate(reviewerDetails.latestCommentTime);
         card.querySelector(".days-since-last-review").innerText = daysSinceLastReview;
+        card.querySelector(".days-in-last-28").innerText = reviewerDetails.recentReviewDays.size;
         card.querySelector(".total-reviewed-prs").innerText = reviewerDetails.prs.size;
-        if (daysSinceLastReview > 30) {
+        if (daysSinceLastReview > 28) {
             card.querySelector(".reviewer-card").classList.add("inactive");
         } else if (daysSinceLastReview < 14 && reviewerDetails.prs.size > 10) {
             card.querySelector(".reviewer-card").classList.add("super-active");
